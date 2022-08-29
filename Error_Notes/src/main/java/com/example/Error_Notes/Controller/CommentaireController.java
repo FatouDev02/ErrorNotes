cd3361cf09ecddbd99121d9a4b7dbdc73751ac01
fccoulibaly <fatoumatacoulibaly4155@gmail.com>
2022-08-29 09:49:25 +0000

    @API&APIOPERATION FC

--- Error_Notes/src/main/java/com/example/Error_Notes/Controller/CommentaireController.java
+++ Error_Notes/src/main/java/com/example/Error_Notes/Controller/CommentaireController.java
@@ -3,6 +3,7 @@ import com.example.Error_Notes.Services.CommentaireService;
 import com.example.Error_Notes.models.Commentaire;
 import com.example.Error_Notes.models.Probleme;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,18 +12,21 @@ import org.springframework.web.bind.annotation.*;
 @Data
 @RequestMapping("/commentaire")
 
-@Api(value = "hello", description = "Methodes sur Commentaire")
+@Api(value = "hello", description = "Methodes  Commentaire")
 
 public class CommentaireController {
     @Autowired
     CommentaireService commentaireService;
     @PostMapping("/add")
+    @ApiOperation(value = "Ajouter un commentaire")
     Commentaire add(@RequestBody Commentaire commentaire){
 
         return commentaireService.Creer(commentaire);
     }
     @DeleteMapping("/delete/{id_Commentaire}")
+    @ApiOperation(value = "Supprimer un commentaire ")
     String delete(@PathVariable Long id_Commentaire){
+
         return commentaireService.Supprimer(id_Commentaire);
     }
 }
