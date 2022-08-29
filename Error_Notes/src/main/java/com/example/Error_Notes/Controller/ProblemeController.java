990589b34dfd58a02a8757bd31f2bb3413c08961
fccoulibaly <fatoumatacoulibaly4155@gmail.com>
2022-08-29 09:37:01 +0000

    @API&APIOPERATION

--- Error_Notes/src/main/java/com/example/Error_Notes/Controller/ProblemeController.java
+++ Error_Notes/src/main/java/com/example/Error_Notes/Controller/ProblemeController.java
@@ -3,6 +3,8 @@ package com.example.Error_Notes.Controller;
 import com.example.Error_Notes.Services.ProblemeService;
 import com.example.Error_Notes.models.Probleme;
 import com.example.Error_Notes.models.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.parsing.Problem;
@@ -13,14 +15,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/probleme")
 @Data
+
+@Api(value = "hello", description = "Methodes Problemes")
+
 public class ProblemeController {
     @Autowired
     ProblemeService problemeService;
-
+    @ApiOperation(value = "Rechercher un probleme par mot-clé ")
     @GetMapping("/search/{mot_cle}")
     Object search(@PathVariable String mot_cle){
         return problemeService.recherche(mot_cle);
     }
+
+    @ApiOperation(value = "Ajouter un Probleme ")
     @PostMapping("/add")
     String add(@RequestBody Probleme probleme,Long idprobleme){
         if(this.problemeService.creer(probleme,idprobleme)==null){
@@ -29,7 +36,7 @@ public class ProblemeController {
         return "probleme ajouté";
     }
 
-
+    @ApiOperation(value = "Modifier un probleme ")
     @PutMapping("/update/{idprobleme}")
     Probleme update(@RequestBody Probleme probleme, @PathVariable Long idprobleme){
         return problemeService.modifier(probleme, idprobleme);
@@ -39,7 +46,7 @@ public class ProblemeController {
 //    Probleme s(@RequestBody Probleme probleme,@PathVariable String motcle){
 //        return problemeService.RechercheP(probleme,motcle);
 //    }
-
+@ApiOperation(value = "Supprimer un probleme ")
     @DeleteMapping("/delete/{idprobleme}")
     String delete(@PathVariable Long idprobleme){
         return problemeService.supprimer(idprobleme);
